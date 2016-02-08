041153b26277020f908d80e72c7ad2c9d8757e3b
Wojciech Sykurski <w.sykurski@samsung.com>
2016-02-08 19:16:26 +0100

    Issue title validation - presence

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,5 +1,7 @@
 class Issue < ActiveRecord::Base
   #belongs_to :creator, :class_name => 'User', :foreign_key => 'creator_id'
+  validates :title, :presence => true
   belongs_to :creator, :class_name => 'User'
   belongs_to :assigned, :class_name => 'User', :foreign_key => 'technican_id'
+  has_many :notes
 end
